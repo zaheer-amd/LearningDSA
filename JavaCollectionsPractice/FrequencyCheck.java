51978f33649b2f23e0cb92eb050164556dec29bf
zaheer-amd <zaheerddl@gmail.com>
2024-10-28 15:54:04 +0530

    TCS IPA Preparation

--- JavaCollectionsPractice/FrequencyCheck.java
+++ JavaCollectionsPractice/FrequencyCheck.java
@@ -1,4 +1,21 @@
 package JavaCollectionsPractice;
 
+import java.util.HashMap;
+
 public class FrequencyCheck {
+    public  static void main(String[] args){
+        int[] input = new int[]{1, 2, 3, 4, 5, 4, 5, 2, 2, 3};
+
+        System.out.println(countFrequency(input));
+    }
+
+    private static HashMap<Integer, Integer> countFrequency(int[] input) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int i: input){
+            hashMap.put(i, hashMap.getOrDefault(i, 0) +1);
+        }
+
+        return hashMap;
+    }
 }
